e95e85425110918e5a3330e7697556c3c7e9d76b
mrinalsarkar1999 <62418152+mrinalsarkar1999@users.noreply.github.com>
2023-10-17 19:17:55 +0530

    Create Meme.js
    Added the main Meme.js file which has the main webpage components as well as the logic to generate random memes from a locally derived data object

---
+++ components/Meme.js
@@ -0,0 +1,33 @@
+import memeData from "../memesData";
+import React, { useState } from "react";
+
+export default function Meme() {
+  const [url, setUrl] = useState("");
+
+  function displayMeme() {
+    const memesArray = memeData.data.memes;
+    const randomNumber = Math.floor(Math.random() * memesArray.length);
+    setUrl(memesArray[randomNumber].url);
+  }
+  console.log(url);
+  return (
+    <main>
+      <div className="form">
+        <div className="inputs">
+          <input type="text" className="input-field" placeholder="Top text" />
+          <input
+            type="text"
+            className="input-field"
+            placeholder="Bottom text"
+          />
+        </div>
+        <button className="form-submit" onClick={displayMeme}>
+          <h3>Get a new meme image</h3>
+        </button>
+      </div>
+      <div className="memeImage">
+        <img src={url} alt="memeImage" id="meme-image" />
+      </div>
+    </main>
+  );
+}
