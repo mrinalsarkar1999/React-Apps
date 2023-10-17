b9312a1461b5f7f25e5a71505c891295a438423c
mrinalsarkar1999 <62418152+mrinalsarkar1999@users.noreply.github.com>
2023-10-17 20:05:04 +0530

    Create CardArray.js
    A repository of card objects that act as local database

---
+++ Airbnb site(practice)/src/components/CardArray.js
@@ -0,0 +1,26 @@
+export default [
+  {
+    image: "Nitish.jpg",
+    rating: 5.0,
+    reviewCount: 90,
+    country: "India",
+    cardTitle: "Life lessons with Nitish",
+    price: 150,
+  },
+  {
+    image: "Nitish.jpg",
+    rating: 5.0,
+    reviewCount: 69,
+    country: "USA",
+    cardTitle: "Life lessons with Amresh",
+    price: 150,
+  },
+  {
+    image: "Nitish.jpg",
+    rating: 4.5,
+    reviewCount: 50,
+    country: "India",
+    cardTitle: "Life lessons with Gaurav",
+    price: 150,
+  },
+];
