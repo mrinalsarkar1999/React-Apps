8fb6044fe0ff80dee29d41cbcf73c37d16f658db
mrinalsarkar1999 <62418152+mrinalsarkar1999@users.noreply.github.com>
2023-10-29 09:07:43 +0530

    Update Header.js

--- Meme generator/components/Header.js
+++ Meme generator/components/Header.js
@@ -5,7 +5,7 @@ export default function Header() {
         <img src="./images/trollface.png" alt="trollFace" className="icon" />
         <h2 className="header-title">Meme Generator</h2>
       </div>
-      <h3 className="header-project">React Course - Project 3</h3>
+      <h3 className="header-project">Need fresh memes? We gotchu covered</h3>
     </header>
   );
 }
