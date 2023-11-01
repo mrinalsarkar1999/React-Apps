b2bd0c0f483e4e72a479f51e917146cf08d8001c
mrinalsarkar1999 <62418152+mrinalsarkar1999@users.noreply.github.com>
2023-11-01 23:43:45 +0530

    Update Card.js

--- Airbnb site(practice)/src/components/Card.js
+++ Airbnb site(practice)/src/components/Card.js
@@ -10,12 +10,15 @@ export default function Card(props) {
 
   return (
     <div className="card">
+	{/*Card badge*/}
       {badgeText && <div className="card-badge">{badgeText}</div>}
+	{/*Card image*/}
       <img
         src={`./icons/${props.coverImg}`}
         className="card-image"
         alt="card profile"
       ></img>
+	  {/*Stats of the card that include ratings*/}
       <div className="card-stats">
         <img src="./icons/star.png" alt="star icon" className="star-img"></img>
         <span className="rating">{props.stats.rating}</span>
@@ -24,6 +27,7 @@ export default function Card(props) {
         </span>
         <span className="country">{props.location}</span>
       </div>
+	  {/*Details of the person*/}
       <div className="card-details">
         <p className="card-title">{props.title}</p>
         <span className="price">From ${props.price}</span>
